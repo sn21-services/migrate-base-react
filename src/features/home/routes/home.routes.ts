70d8281babeece9247d875d837c3263595d0f01a
Sonle <sn21.services@gmail.com>
2026-07-13 23:36:01 +0700

    feat: init config tailwind

--- src/features/home/routes/home.routes.ts
+++ src/features/home/routes/home.routes.ts
@@ -2,6 +2,7 @@ import { lazy } from 'react';
 
 import type { RouteItemDef } from '@/types/routes.types';
 import { HomePathsEnum } from '@/features/home/constants/home.paths';
+import BlankLayout from '@/layouts/BlankLayout';
 
 const HOME_SCREEN: RouteItemDef = {
   id: 'HOME',
@@ -13,6 +14,7 @@ const TODO_SCREEN: RouteItemDef = {
   id: 'TODO',
   path: '/todo',
   Component: lazy(() => import('@/features/home/screens/TodoScreen')),
+  layout: BlankLayout,
 };
 
 export const HOME_ROUTES = [HOME_SCREEN, TODO_SCREEN];
